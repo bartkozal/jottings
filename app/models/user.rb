1438d9c8c1f8ba61fc65f88f09efc7034c51f3fd
Bartlomiej Kozal <bkzl@me.com>
2016-10-12 21:16:12 +0200

    Replace email with avatar in navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  def gravatar(size: 45)
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+  end
 end
