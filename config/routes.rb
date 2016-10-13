09aa21d1aa93fffc91eafb83fca3cc87bd921239
Bartlomiej Kozal <bkzl@me.com>
2016-10-13 18:01:47 +0200

    List notes in sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   constraints Clearance::Constraints::SignedIn.new do
     root to: "editor/notes#new"
+
+    namespace :editor do
+      resources :notes, except: :index
+    end
   end
 
   constraints Clearance::Constraints::SignedOut.new do
