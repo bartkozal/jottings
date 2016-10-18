c2d61e6168fb47c9c70033c32e9ee9149a34e74b
Bartlomiej Kozal <bkzl@me.com>
2016-10-19 00:12:40 +0200

    Revert 09cdef4

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ApplicationRecord
 
   has_many :collaborations
   has_many :documents, through: :collaborations
-
-  def to_param
-    MaskedId.encode(id)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resource :session, controller: "clearance/sessions", only: [:create]
 
   resources :users, controller: "clearance/users", only: [:create] do
-    resource :password, controller: "passwords", only: [:create, :edit, :update]
+    resource :password, controller: "clearance/passwords", only: [:create, :edit, :update]
   end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
