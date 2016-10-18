208671516b2037128f534d67ee2a18c6b63b5058
Bartlomiej Kozal <bkzl@me.com>
2016-10-18 20:43:12 +0200

    Add test for creating new document

--- test/integration/create_document_test.rb
+++ test/integration/create_document_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class CreateDocumentTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = create(:user)
+  end
+
   test "sanity" do
-    skip
+    visit root_path(as: @user)
+    click_button "New document"
+    within ".editor-sidebar-item" do
+      assert page.has_content?("Untitled")
+    end
+    assert_equal editor_document_path(@user.documents.last), current_path
   end
 end
