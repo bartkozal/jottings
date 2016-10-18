7497cadeeb49adb7b2c2aae121d19f8cbcac91a3
Bartlomiej Kozal <bkzl@me.com>
2016-10-18 02:24:58 +0200

    Ensure secure cookie on production

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,6 +2,10 @@ Clearance.configure do |config|
   config.routes = false
   config.cookie_domain = ".jottins.co"
   config.mailer_sender = "support@jottings.co"
+
+  unless Rails.env.development? || Rails.env.test?
+    config.secure_cookie = true
+  end
 end
 
 Clearance::PasswordsController.layout "clearance"
