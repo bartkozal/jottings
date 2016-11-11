4836cbe9288c45ea600714a9705331b904e8c20c
Bartlomiej Kozal <bkzl@me.com>
2016-11-11 14:48:14 +0100

    Allow to drag document

--- app/assets/javascripts/components/sidebar/item_document.js
+++ app/assets/javascripts/components/sidebar/item_document.js
@@ -1,6 +1,30 @@
 Vue.component('sidebar-item-document', {
   mounted() {
+    interact(this.$el, {
+      styleCursor: false
+    }).draggable({
+      autoScroll: true,
+      onstart(event) {
+        event.target.classList.add("is-dragged");
+      },
+      onmove(event) {
+        let target = event.target,
+            x = +(target.dataset.x || 0) + event.dx,
+            y = +(target.dataset.y || 0) + event.dy;
 
+        target.style.transform = `translate(${x}px, ${y}px)`;
+        target.dataset.x = x;
+        target.dataset.y = y;
+      },
+      onend(event) {
+        let target = event.target;
+
+        target.classList.remove("is-dragged");
+        target.style.transform = null;
+        target.dataset.x = 0;
+        target.dataset.y = 0;
+      }
+    });
   },
   template: `
     <li>
