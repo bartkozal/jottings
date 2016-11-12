e8e91fa6d2023f4a8aa826637e541bb42a45fc02
Bartlomiej Kozal <bkzl@me.com>
2016-11-13 00:24:48 +0100

    Add actions authorization for moving documents

--- app/controllers/editor/documents/moves_controller.rb
+++ app/controllers/editor/documents/moves_controller.rb
@@ -1,11 +1,9 @@
 class Editor::Documents::MovesController < EditorController
   before_action :find_document
   before_action :find_stack, only: :create
-  # before_action :require_ownership
-
-  # TODO
-  # Don't allow to move documents out of shared stack if not an owner
-  # Don't allow to move shared documents into shared stacks
+  before_action :require_stack_ownership, only: :destroy
+  before_action :require_stack_ownership, only: :create, if: -> { @document.stack.present? }
+  before_action :require_no_collaborators, only: :create, if: -> { @document.stack.blank? }
 
   def create
     @document.stack = @stack
@@ -29,5 +27,13 @@ class Editor::Documents::MovesController < EditorController
   def find_stack
     decoded_id = MaskedId.decode(:stack, params[:id])
     @stack = current_user.stacks.find(decoded_id)
+  end
+
+  def require_stack_ownership
+    raise ApplicationController::NotAuthorized unless @document.stack.owner?(current_user)
+  end
+
+  def require_no_collaborators
+    raise ApplicationController::NotAuthorized if @document.has_collaborators?
   end
 end
--- test/integration/move_document_test.rb
+++ test/integration/move_document_test.rb
@@ -4,4 +4,16 @@ class MoveDocumentTest < ActionDispatch::IntegrationTest
   test "moving document to stack" do
     skip
   end
+
+  test "disallowing to move shared document to stack" do
+    skip
+  end
+
+  test "disallowing to move shared document out of stack on lack of ownership" do
+    skip
+  end
+
+  test "disallowing to move shared document between stacks on lack of ownership" do
+    skip
+  end
 end
