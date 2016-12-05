c91b6c73e466b7609471e3a5baa91a8e56c1e7c6
Bartlomiej Kozal <bkzl@me.com>
2016-12-05 17:39:56 +0100

    Add tooltip to explain how ownership works

---
+++ app/assets/javascripts/components/tooltip.js
@@ -0,0 +1,26 @@
+Vue.component("tooltip", {
+  props: ['width', 'offset'],
+  data() {
+    return {
+      isVisible: false
+    };
+  },
+  computed: {
+    styleAttr() {
+      return `width: ${this.width}px; left: ${this.offset || 0}px`;
+    }
+  },
+  template: `
+    <span class="has-tooltip">
+      <span @mouseover="isVisible = true" @mouseleave="isVisible = false">
+        <slot name="tooltip-trigger"></slot>
+      </span>
+
+      <transition name="transition-fade">
+        <div class="tooltip" v-if="isVisible" :style="styleAttr">
+          <slot></slot>
+        </div>
+      </transition>
+    </span>
+  `
+});
