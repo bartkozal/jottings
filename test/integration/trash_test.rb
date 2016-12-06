656d3c2d09163c58845b037a4e4cfdf9f38f0bad
Bartlomiej Kozal <bkzl@me.com>
2016-12-06 21:08:03 +0100

    Add empty tests for the trash

---
+++ test/integration/trash_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class TrashTest < ActionDispatch::IntegrationTest
+  test "removing document moves it to the trash" do
+    skip
+  end
+
+  test "removing stack with documents only shows stack in the trash" do
+    skip
+  end
+
+  test "seeing stack in trash only by the owner" do
+    skip
+  end
+
+  test "restoring removed document" do
+    skip
+  end
+
+  test "removing document permanently" do
+    skip
+  end
+
+  test "restoring removed stack" do
+    skip
+  end
+
+  test "removing stack permanently" do
+    skip
+  end
+
+  test "empting trash" do
+    skip
+  end
+end
