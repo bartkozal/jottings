d18f30608f8b260f4f617d3472f1ba96d82b0561
Bartlomiej Kozal <bkzl@me.com>
2016-12-07 02:47:26 +0100

    Add trash tests

--- test/integration/trash_test.rb
+++ test/integration/trash_test.rb
@@ -2,34 +2,119 @@ require "test_helper"
 
 class TrashTest < ActionDispatch::IntegrationTest
   test "removing document moves it to the trash" do
-    skip
+    user = create(:user)
+    document = create(:document, user: user)
+
+    visit root_path(as: user)
+    click_link "Remove"
+    click_link "Trash"
+    within ".editor-content" do
+      assert page.has_content?(document.name)
+    end
   end
 
   test "removing stack with documents only shows stack in the trash" do
-    skip
+    user = create(:user)
+    stack = create(:stack, assign_to: user)
+    document = create(:document, user: user, stack: stack)
+
+    visit root_path(as: user)
+    click_link "Remove", match: :first
+    click_link "Trash"
+    within ".editor-content" do
+      refute page.has_content?(document.name)
+      assert page.has_content?(stack.name)
+    end
   end
 
   test "seeing stack in trash only by the owner" do
-    skip
+    user_a = create(:user, has_stack: true)
+    user_b = create(:user)
+    stack = user_a.stacks.last
+    stack.collaborators << user_b
+
+    visit root_path(as: user_a)
+    click_link "Remove"
+    click_link "Trash"
+    within ".editor-content" do
+      assert page.has_content?(stack.name)
+    end
+
+    visit root_path(as: user_b)
+    click_link "Trash"
+    within ".editor-content" do
+      refute page.has_content?(stack.name)
+    end
   end
 
   test "restoring removed document" do
-    skip
+    user = create(:user, has_document: true)
+    document = user.documents.last
+
+    visit root_path(as: user)
+    click_link "Remove"
+    click_link "Trash"
+    click_link "Restore"
+
+    within ".editor-sidebar" do
+      assert page.has_content?(document.name)
+    end
   end
 
   test "removing document permanently" do
-    skip
+    user = create(:user, has_document: true)
+    document = user.documents.last
+
+    visit root_path(as: user)
+    click_link "Remove"
+    click_link "Trash"
+    click_link "Delete Forever"
+
+    within ".editor-content" do
+      refute page.has_content?(document.name)
+    end
   end
 
   test "restoring removed stack" do
-    skip
+    user = create(:user, has_stack: true)
+    stack = user.stacks.last
+
+    visit root_path(as: user)
+    click_link "Remove"
+    click_link "Trash"
+    click_link "Restore"
+
+    within ".editor-sidebar" do
+      assert page.has_content?(stack.name)
+    end
   end
 
   test "removing stack permanently" do
-    skip
+    user = create(:user, has_stack: true)
+    stack = user.stacks.last
+
+    visit root_path(as: user)
+    click_link "Remove"
+    click_link "Trash"
+    click_link "Delete Forever"
+
+    within ".editor-content" do
+      refute page.has_content?(stack.name)
+    end
   end
 
   test "empting trash" do
-    skip
+    user = create(:user, has_document: true, has_stack: true)
+    stack = user.stacks.last
+    document = user.documents.last
+
+    visit root_path(as: user)
+    click_link "Remove", match: :first
+    click_link "Remove"
+    click_link "Trash"
+    click_link "Empty"
+
+    refute page.has_content?(stack.name)
+    refute page.has_content?(document.name)
   end
 end
