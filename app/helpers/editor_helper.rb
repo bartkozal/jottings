6bf536ce2765e9b203764a375d65699044d71b6d
Bartlomiej Kozal <bkzl@me.com>
2016-12-19 01:29:57 +0100

    Mark selected document

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -2,4 +2,8 @@ module EditorHelper
   def open_new_stack_modal?
     current_page?(root_path) && !cookies.signed[:last_visited_document]
   end
+
+  def active_document?(document)
+    params[:id] == document.to_param
+  end
 end
