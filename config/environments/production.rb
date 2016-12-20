5660eb01b9a2da257bceb3792cdb0b7afe1e1c6d
Bartlomiej Kozal <bkzl@me.com>
2016-12-20 17:26:05 +0100

    Fix for production action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://jottings.co/cable'
+  # config.action_cable.url = 'ws://jottings.co/cable'
   config.action_cable.allowed_request_origins = [ 'https://jottings.co', /https:\/\/jottings.co.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
