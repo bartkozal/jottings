02934aab813b536fe04bdac60a1bf15eb388e0ab
Bartlomiej Kozal <bkzl@me.com>
2016-12-20 16:36:10 +0100

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,10 +37,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://jottings.co/cable'
-  config.action_cable.allowed_request_origins = [ 'https://jottings.co', /http:\/\/jottings.co.*/]
+  config.action_cable.allowed_request_origins = [ 'https://jottings.co', /https:\/\/jottings.co.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
