48e1a3a4d32506b6691ff07dc35e85e93d795951
Bartlomiej Kozal <bkzl@me.com>
2016-12-21 15:31:54 +0100

    Limit recipients of feedback notification

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,7 +2,7 @@ class FeedbackMailer < ApplicationMailer
   def notify(feedback)
     @feedback = feedback
 
-    mail to: ["bkzl@me.com", "mackozal@me.com"],
+    mail to: "bkzl@me.com",
          subject: %(New feedback on Jottings)
   end
 end
